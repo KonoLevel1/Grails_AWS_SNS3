405f1b969fab2043244e3945d8e51d98b04f439e
Junichi Koyama <koyama@rightcode.co.jp>
2020-10-13 16:46:12 +0900

    構成図の差し替え

---
+++ DeviceTokenGet/DeviceTokenGet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeviceTokenGet
+//
+//  Created by 小山順一 on 2020/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
